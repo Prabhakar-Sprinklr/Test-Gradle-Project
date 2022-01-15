a798656b149770c97c4aff6fb0afa433182d694e
Prabhakar Kumar <prabhakar.kumar@sprinklr.com>
2022-01-15 10:59:06 +0530

    Final Commit

--- src/main/java/test_package/test_class.java
+++ src/main/java/test_package/test_class.java
@@ -6,5 +6,7 @@ public class test_class {
         System.out.println("This is first gradle build.");
         System.out.println("This is When I commit i guess third.");
         System.out.println("This is When I commit i guess third again.");
+        System.out.println("./gradlew build to build in directory of the build file");
+        System.out.println("./gradlew run to run the application");
     }
 }
