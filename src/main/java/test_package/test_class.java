2671d9cea04aceac202b80f25a4416c7ef2de30a
Prabhakar Kumar <prabhakar.kumar@sprinklr.com>
2022-01-15 10:24:45 +0530

    First Commit

---
+++ src/main/java/test_package/test_class.java
@@ -0,0 +1,8 @@
+package test_package;
+
+public class test_class {
+    public static void main(String[] args) {
+        System.out.println("Hello World\n");
+        System.out.println("This is first gradle build.");
+    }
+}
